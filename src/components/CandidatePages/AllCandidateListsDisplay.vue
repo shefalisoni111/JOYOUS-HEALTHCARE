c161dc80fa8b08b324b80b70c35a40124bfe5ccc
shefali <shefali.s@pixoatic.com>
2024-08-30 16:52:22 +0530

    updated code

--- src/components/CandidatePages/AllCandidateListsDisplay.vue
+++ src/components/CandidatePages/AllCandidateListsDisplay.vue
@@ -62,7 +62,7 @@
                   <i class="bi bi-person-circle"></i>
                 </button>
               </td>
-              <td>{{ candidate.last_login }}</td>
+              <td>{{ formatDate(candidate.last_login) }}</td>
               <td class="cursor-pointer">
                 <router-link
                   class="btn btn-outline-success text-nowrap"
@@ -145,6 +145,19 @@ export default {
     // },
   },
   methods: {
+    formatDate(dateString) {
+      const date = new Date(dateString);
+
+      const year = new Date().getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const day = String(date.getDate()).padStart(2, "0");
+
+      const hours = String(date.getHours()).padStart(2, "0");
+      const minutes = String(date.getMinutes()).padStart(2, "0");
+      const seconds = String(date.getSeconds()).padStart(2, "0");
+
+      return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+    },
     async getCandidateMethods() {
       this.isLoading = true;
       try {
