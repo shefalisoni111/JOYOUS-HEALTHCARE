3a33cfedff30a8f0209a2114acc41214db48019d
shefali <shefali.s@pixoatic.com>
2025-02-19 11:51:22 +0530

    updated code for invoice

--- src/components/ReportPages/InvoiceReport.vue
+++ src/components/ReportPages/InvoiceReport.vue
@@ -192,7 +192,7 @@
                           <th scope="col">Site</th>
                           <!-- <th scope="col">Employee Name</th>
                           <th scope="col">Job</th> -->
-                          <th scope="col">Shift Date</th>
+                          <!-- <th scope="col">Shift Date</th> -->
                           <!-- <th scope="col">Payment Ref</th> -->
                           <th scope="col">Time From</th>
                           <th scope="col">Time To</th>
@@ -212,8 +212,8 @@
                             {{ data.candidate_data?.[index]?.can_name }}
                           </td>
                           <td scope="col">{{ data.candidate_data?.[0]?.job }}</td> -->
+                          <!-- <td scope="col">{{ data.start_date }}</td> -->
                           <td scope="col">{{ data.start_date }}</td>
-                          <td scope="col">{{ data.end_date }}</td>
                           <td scope="col">{{ data.end_date }}</td>
                           <td scope="col">
                             {{ data.candidate_data?.[0]?.total_hours }}
@@ -233,12 +233,12 @@
                       </tbody>
                       <tbody v-else>
                         <tr v-if="errorMessageFilter">
-                          <td colspan="9" class="text-danger text-center">
+                          <td colspan="8" class="text-danger text-center">
                             {{ errorMessageFilter }}
                           </td>
                         </tr>
                         <tr v-else>
-                          <td colspan="9" class="text-danger text-center">
+                          <td colspan="8" class="text-danger text-center">
                             {{ errorMessageCustom }}
                           </td>
                         </tr>
@@ -293,7 +293,7 @@
                           <th scope="col">Site</th>
                           <!-- <th scope="col">Employee Name</th> -->
                           <!-- <th scope="col">Job</th> -->
-                          <th scope="col">Shift Date</th>
+                          <!-- <th scope="col">Shift Date</th> -->
                           <!-- <th scope="col">Payment Ref</th> -->
                           <th scope="col">Time From</th>
                           <th scope="col">Time To</th>
@@ -313,8 +313,8 @@
                             {{ data.candidate_data?.[index]?.can_name }}
                           </td>
                           <td scope="col">{{ data.candidate_data?.[0]?.job }}</td> -->
+                          <!-- <td scope="col">{{ data.start_date }}</td> -->
                           <td scope="col">{{ data.start_date }}</td>
-                          <td scope="col">{{ data.end_date }}</td>
                           <td scope="col">{{ data.end_date }}</td>
                           <td scope="col">
                             {{ data.candidate_data?.[0]?.total_hours }}
@@ -334,7 +334,7 @@
                       </tbody>
                       <tbody v-else>
                         <tr>
-                          <td colspan="9" class="text-danger text-center">
+                          <td colspan="8" class="text-danger text-center">
                             {{ errorMessage }}
                           </td>
                         </tr>
