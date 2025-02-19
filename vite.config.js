e9130a6006aa0cca1e98360c632539cec24286c1
shefali <shefali.s@pixoatic.com>
2025-02-19 17:10:42 +0530

    update code

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,45 @@
+// import { defineConfig } from 'vite';
+// import vue from '@vitejs/plugin-vue';
+
+// const API_URL = 'https://recpalapp.co.uk/api';
+// // const API_URL = 'http://194.31.150.133:3000';
+
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': '/src',
+//     },
+//   },
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: API_URL, 
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//     },
+//     server: {
+//       watch: {
+//         ignored: ['**/vite.config.js'],
+//       },
+//     },
+//     allowedHosts: ['recpalapp.co.uk'], 
+//   },
+//   define: {
+//       VITE_API_URL: JSON.stringify(API_URL),
+//   },
+//   optimizeDeps: {
+//     include: ['vue'],
+//     exclude: ['vue-demi'],
+//   },
+ 
+// });
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 const API_URL = 'https://recpalapp.co.uk/api';
-// const API_URL = 'http://194.31.150.133:3000';
 
 export default defineConfig({
   plugins: [vue()],
@@ -14,24 +51,20 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: API_URL, 
+        target: API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-    server: {
-      watch: {
-        ignored: ['**/vite.config.js'],
-      },
+    watch: {
+      ignored: ['**/vite.config.js'],
     },
-    allowedHosts: [`API_URL`], 
+    host: true,  
+    strictPort: true,
+    allowedHosts: ['.recpalapp.co.uk'], 
   },
   define: {
-      VITE_API_URL: JSON.stringify(API_URL),
+    VITE_API_URL: JSON.stringify(API_URL),
   },
-  optimizeDeps: {
-    include: ['vue'],
-    exclude: ['vue-demi'],
-  },
- 
 });
+
